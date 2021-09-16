2df1cded63346529e784f314500af6db3d093f2d
lancetoledo <lanceadrian13@gmail.com>
2021-09-16 19:44:28 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcisstic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+//    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+
 function narcissistic(value) {
   // Code me to return true or false
   let str = value.toString();
